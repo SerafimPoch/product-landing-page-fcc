52c426f40baf003773726938d1fb630d81018c48
SerafimPoch <serpochapinsky@gmail.com>
2018-09-27 14:01:22 +0300

    add links to the top

--- src/components/Footer/footerNavbar/index.js
+++ src/components/Footer/footerNavbar/index.js
@@ -1,13 +1,12 @@
 import React from "react";
 import { FooterNavbar } from "./style";
-import { Link } from "react-router-dom";
 
 export default () => {
   return (
     <FooterNavbar>
-      <Link to="privacy">Privacy</Link>
-      <Link to="terms">Terms</Link>
-      <Link to="contact">Contact</Link>
+      <a href="">Privacy</a>
+      <a href="">Terms</a>
+      <a href="">Contact</a>
     </FooterNavbar>
   );
 };
