696296b169869150bc009b5d78fcdf22ea7b73a4
SerafimPoch <serpochapinsky@gmail.com>
2018-09-27 16:23:36 +0300

    change title

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -2,10 +2,14 @@ import React from "react";
 import Trombone from "../../components/Header/trombone";
 import Navbar from "../../components/Header/navbar";
 import { HeaderContainer } from "./style";
+import { Helmet } from "react-helmet";
 
 export default () => {
   return (
     <HeaderContainer>
+      <Helmet>
+        <title>Product Landing Page</title>
+      </Helmet>
       <Trombone />
       <Navbar />
     </HeaderContainer>
